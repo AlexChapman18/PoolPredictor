b48180b72cd8acdca4b1e02023ad8bea2e1ba271
semajson <jamessandy95@gmail.com>
2020-12-31 20:22:54 +0000

    more attempts

--- process_image_james.py
+++ process_image_james.py
@@ -76,14 +76,15 @@ cap.set(3, 1280)
 cap.set(4, 720)
 
 # Trying to find the corners of the board
-if True:
+while True:
+
+    table = cv2.imread("table_far_out.jpg")
+    dst = table
+
     # ret, frame = cap.read()
     # table = frame
 
-    table = cv2.imread("table_angle.jpg")
-    dst = table
-
-    # # undistort
+    # undistort
     # h, w = table.shape[:2]
     # newcameramtx, roi = cv2.getOptimalNewCameraMatrix(MTX, DIST, (w, h), 1, (w, h))
     # dst = cv2.undistort(table, MTX, DIST, None, newcameramtx)
@@ -103,49 +104,58 @@ if True:
     lower_all = np.array([125, 78, 73])
     upper_all = np.array([143, 160, 176])
     mask2 = cv2.inRange(hsv, lower_all, upper_all)
-    cv2.imshow("mask2", mask2)
+    cv2.imshow("mask2", cv2.resize(mask2, (960, 540)))
 
     contours = cv2.findContours(
         mask2.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
     )[-2]
     contour = max(contours, key=cv2.contourArea)
-    # img2 = cv2.drawContours(dst, [contour], -1, (0, 255, 0), 3)
+    img1 = dst
+    # img1 = cv2.drawContours(dst, [contour], -1, (0, 255, 0), 3)
 
-    hull = cv2.convexHull(contour)
-    img2 = cv2.drawContours(dst, [hull], -1, (0, 100, 100), 3)
+    rect = cv2.minAreaRect(contour)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    img1_5 = cv2.drawContours(dst, [box], 0, (0, 100, 100), 2)
+
+    # img2 = cv2.drawContours(dst, [hull], -1, (0, 100, 100), 3)
+
+    epsilon = 0.0045 * cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(contour, epsilon, True)
+    img2 = cv2.drawContours(img1_5, [approx], 0, (160, 255, 0), 2)
 
     # Find the corners of the table
     # Corners are annoying nested with pointless arrays, also it must by numpy arrays...
+    contour = [i[0] for i in contour]
+    (x_max, x_min), (y_max, y_min) = [(max(i), min(i)) for i in zip(*contour)]
+
+    # Remove the skirting
+    y_max_pix = y_max - y_min
+    x_max_pix = x_max - x_min
+    x_skirt = x_max_pix * (SKIRT / X_MAX)
+    y_skirt = y_max_pix * (SKIRT / Y_MAX)
+
+    corners_1 = [
+        [x_min, y_min],
+        [x_min, y_max],
+        [x_max, y_max],
+        [x_max, y_min],
+    ]
+
+    corners = [
+        [int(x_min + x_skirt), int(y_min + y_skirt)],
+        [int(x_min + x_skirt), int(y_max - y_skirt)],
+        [int(x_max - x_skirt), int(y_max - y_skirt)],
+        [int(x_max - x_skirt), int(y_min + y_skirt)],
+    ]
+
     try:
-        contour = [i[0] for i in contour]
-        (x_max, x_min), (y_max, y_min) = [(max(i), min(i)) for i in zip(*contour)]
-
-        # Remove the skirting
-        y_max_pix = y_max - y_min
-        x_max_pix = x_max - x_min
-        x_skirt = x_max_pix * (SKIRT / X_MAX)
-        y_skirt = y_max_pix * (SKIRT / Y_MAX)
-
-        corners_1 = [
-            [x_min, y_min],
-            [x_min, y_max],
-            [x_max, y_max],
-            [x_max, y_min],
-        ]
-
-        corners = [
-            [int(x_min + x_skirt), int(y_min + y_skirt)],
-            [int(x_min + x_skirt), int(y_max - y_skirt)],
-            [int(x_max - x_skirt), int(y_max - y_skirt)],
-            [int(x_max - x_skirt), int(y_min + y_skirt)],
-        ]
-
         cv2.drawContours(img2, [np.array(corners)], 0, (255, 255, 255), 2)
         cv2.drawContours(img2, [np.array(corners_1)], 0, (0, 255, 255), 2)
 
         cv2.imshow("test", cv2.resize(img2, (960, 540)))
 
-        cv2.waitKey(0)
-    except IndexError:
+        cv2.waitKey(1)
+    except:
         print("error")
 input()
