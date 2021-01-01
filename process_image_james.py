0d89b3f68398417ca4d312293d7d08865e0b56d2
semajson <jamessandy95@gmail.com>
2021-01-01 14:47:40 +0000

    commit before delete

--- process_image_james.py
+++ process_image_james.py
@@ -6,32 +6,32 @@ X_MAX = 83.5
 Y_MAX = 167.5
 SKIRT = 3.25
 
-table = cv2.imread("table_with_bands.jpg")
+# table = cv2.imread("table_with_bands.jpg")
 
-# filter image
-bilateral_filtered_image = cv2.bilateralFilter(table, 5, 175, 175)
+# # filter image
+# bilateral_filtered_image = cv2.bilateralFilter(table, 5, 175, 175)
 
-# Convert BGR to HSV
-hsv = cv2.cvtColor(bilateral_filtered_image, cv2.COLOR_BGR2HSV)
+# # Convert BGR to HSV
+# hsv = cv2.cvtColor(bilateral_filtered_image, cv2.COLOR_BGR2HSV)
 
-# define range of blue color in HSV
-# lower_blue = np.array([100, 50, 50])
-# upper_blue = np.array([130, 255, 255])
+# # define range of blue color in HSV
+# # lower_blue = np.array([100, 50, 50])
+# # upper_blue = np.array([130, 255, 255])
 
-# white
-# lower_blue = np.array([0, 0, 93])
-# upper_blue = np.array([255, 35, 255])
-lower_blue = np.array([107, 0, 162])
-upper_blue = np.array([182, 39, 255])
+# # white
+# # lower_blue = np.array([0, 0, 93])
+# # upper_blue = np.array([255, 35, 255])
+# lower_blue = np.array([107, 0, 162])
+# upper_blue = np.array([182, 39, 255])
 
-# Threshold the HSV image to get only blue colors
-mask = cv2.inRange(hsv, lower_blue, upper_blue)
-bluecnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+# # Threshold the HSV image to get only blue colors
+# mask = cv2.inRange(hsv, lower_blue, upper_blue)
+# bluecnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
-if len(bluecnts) > 0:
-    blue_area = max(bluecnts, key=cv2.contourArea)
-    (xg, yg, wg, hg) = cv2.boundingRect(blue_area)
-    cv2.rectangle(table, (xg, yg), (xg + wg, yg + hg), (0, 255, 0), 2)
+# if len(bluecnts) > 0:
+#     blue_area = max(bluecnts, key=cv2.contourArea)
+#     (xg, yg, wg, hg) = cv2.boundingRect(blue_area)
+#     cv2.rectangle(table, (xg, yg), (xg + wg, yg + hg), (0, 255, 0), 2)
 
 
 # cv2.imshow("table", table)
@@ -76,9 +76,9 @@ cap.set(3, 1280)
 cap.set(4, 720)
 
 # Trying to find the corners of the board
-while True:
+if False:
 
-    table = cv2.imread("table_far_out.jpg")
+    table = cv2.imread("corners_and_player_no_hair.jpg")
     dst = table
 
     # ret, frame = cap.read()
@@ -101,62 +101,90 @@ while True:
     # Get outline of the table
     # lower_all = np.array([130, 78, 73])
     # upper_all = np.array([143, 160, 157])
-    lower_all = np.array([125, 78, 73])
-    upper_all = np.array([143, 160, 176])
-    mask2 = cv2.inRange(hsv, lower_all, upper_all)
-    cv2.imshow("mask2", cv2.resize(mask2, (960, 540)))
+    lower_corner = np.array([47, 35, 128])
+    upper_corner = np.array([86, 86, 236])
+
+    mask = cv2.inRange(hsv, lower_corner, upper_corner)
+    cv2.imshow("corners", cv2.resize(mask, (960, 540)))
 
     contours = cv2.findContours(
-        mask2.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
     )[-2]
-    contour = max(contours, key=cv2.contourArea)
-    # img1 = dst
-    img1 = cv2.drawContours(dst, [contour], -1, (0, 255, 0), 3)
 
-    rect = cv2.minAreaRect(contour)
-    box = cv2.boxPoints(rect)
-    box = np.int0(box)
-    img1_5 = cv2.drawContours(dst, [box], 0, (0, 100, 100), 2)
+    contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
 
-    hull = cv2.convexHull(contour)
-    img1_6 = cv2.drawContours(dst, [hull], -1, (200, 100, 100), 3)
-
-    epsilon = 0.05 * cv2.arcLength(hull, True)
-    approx = cv2.approxPolyDP(hull, epsilon, True)
-    img2 = cv2.drawContours(img1_6, [approx], 0, (160, 255, 0), 2)
-
-    # Find the corners of the table
-    # Corners are annoying nested with pointless arrays, also it must by numpy arrays...
-    contour = [i[0] for i in contour]
-    (x_max, x_min), (y_max, y_min) = [(max(i), min(i)) for i in zip(*contour)]
-
-    # Remove the skirting
-    y_max_pix = y_max - y_min
-    x_max_pix = x_max - x_min
-    x_skirt = x_max_pix * (SKIRT / X_MAX)
-    y_skirt = y_max_pix * (SKIRT / Y_MAX)
-
-    corners_1 = [
-        [x_min, y_min],
-        [x_min, y_max],
-        [x_max, y_max],
-        [x_max, y_min],
-    ]
+    img1 = cv2.drawContours(bilateral_filtered_image, contours[:4], -1, (0, 255, 0), 3)
+    cv2.imshow("corners found", cv2.resize(img1, (960, 540)))
+    cv2.waitKey(0)
 
     corners = [
-        [int(x_min + x_skirt), int(y_min + y_skirt)],
-        [int(x_min + x_skirt), int(y_max - y_skirt)],
-        [int(x_max - x_skirt), int(y_max - y_skirt)],
-        [int(x_max - x_skirt), int(y_min + y_skirt)],
+        contours[0],
+        contours[1],
+        contours[2],
+        contours[3],
     ]
 
+    cv2.drawContours(img2, [np.array(corners)], 0, (255, 255, 255), 2)
+
+    cv2.imshow("test", cv2.resize(img2, (960, 540)))
+
+    cv2.waitKey(0)
+
     try:
-        cv2.drawContours(img2, [np.array(corners)], 0, (255, 255, 255), 2)
-        cv2.drawContours(img2, [np.array(corners_1)], 0, (0, 255, 255), 2)
-
-        cv2.imshow("test", cv2.resize(img2, (960, 540)))
-
-        cv2.waitKey(1)
+        pass
     except:
         print("error")
-input()
+
+
+def process_image(image):
+
+    # # undistort
+    # h, w = table.shape[:2]
+    # newcameramtx, roi = cv2.getOptimalNewCameraMatrix(MTX, DIST, (w, h), 1, (w, h))
+    # dst = cv2.undistort(table, MTX, DIST, None, newcameramtx)
+    # x, y, w, h = roi
+    # dst = dst[y : y + h, x : x + w]
+    # cv2.imshow("calibresult_table.png", dst)
+
+    # Blur image
+    blurred_image = cv2.bilateralFilter(image, 5, 175, 175)
+
+    find_board(blurred_image)
+
+
+def find_board(image):
+    find_corners(image)
+
+
+def find_corners(image):
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    lower_color = np.array([47, 35, 128])
+    upper_color = np.array([86, 86, 236])
+
+    mask = cv2.inRange(hsv, lower_color, upper_color)
+    cv2.imshow("corners", cv2.resize(mask, (960, 540)))
+
+    contours = cv2.findContours(
+        mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )[-2]
+
+    contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+
+    img1 = cv2.drawContours(image, contours[:4], -1, (0, 255, 0), 3)
+    cv2.imshow("corners found", cv2.resize(img1, (960, 540)))
+    cv2.waitKey(0)
+
+
+def convert_pixels_to_coords(pixel_x, pixel_y):
+    pass
+
+
+def find_balls(image, color):
+    pass
+
+
+table = cv2.imread("corners_and_player_no_hair.jpg")
+
+process_image(table)
